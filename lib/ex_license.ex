6280b216348d35d7253eb3f18e95af9fec1b1766
Samuel <samuel@uniris.io>
2022-02-22 16:11:16 +0100

    Support option choose the license

--- lib/ex_license.ex
+++ lib/ex_license.ex
@@ -2,4 +2,19 @@ defmodule ExLicense do
   @moduledoc """
   Documentation for `ExLicense`.
   """
+
+  @type available_license() ::
+          :mit | :agpl_v3 | :gpl_v3 | :lgpl_v3 | :mozilla_v2 | :apache_v2 | :boost_v1
+
+  @doc """
+  Return the SPDX License identifier
+  """
+  @spec spdx_id(available_license()) :: String.t()
+  def spdx_id(:mit), do: "MIT"
+  def spdx_id(:agpl_v3), do: "AGPL-3.0-or-later"
+  def spdx_id(:gpl_v3), do: "GPL-3.0+"
+  def spdx_id(:lgpl_v3), do: "LGPL-3.0+"
+  def spdx_id(:mozilla_v2), do: "MPL-2.0"
+  def spdx_id(:apache_v2), do: "Apache-2.0"
+  def spdx_id(:boost_v1), do: "BSL-1.0"
 end
--- lib/mix/tasks/insert_license.ex
+++ lib/mix/tasks/insert_license.ex
@@ -1,14 +1,22 @@
 defmodule Mix.Tasks.InsertLicense do
   use Mix.Task
 
-  @header """
-  # SPDX-License-Identifier: AGPL-3.0-or-later
-  """
+  def run(opts \\ []) do
+    case OptionParser.parse(opts, strict: [license: :string, verify: :boolean]) do
+      {[verify: true, license: license], _argv, _errors} ->
+        verify(String.to_existing_atom(license))
 
-  def run(["--verify"]) do
+      {[{:license, license} | _], _argv, _errors} ->
+        insert(String.to_existing_atom(license))
+    end
+  end
+
+  defp verify(license) do
     files = lib_files()
 
-    if Enum.all?(files, &have_license_header?/1) do
+    license_id = ExLicense.spdx_id(license)
+
+    if Enum.all?(files, &have_license_header?(&1, license_id)) do
       exit({:shutdown, 0})
     else
       Mix.shell().error("Files do not start with the license header")
@@ -17,12 +25,13 @@ defmodule Mix.Tasks.InsertLicense do
     end
   end
 
-  def run(_) do
+  defp insert(license) do
     files = lib_files()
+    license_id = ExLicense.spdx_id(license)
 
     files
-    |> Enum.reject(&have_license_header?/1)
-    |> Enum.each(&insert_license_header/1)
+    |> Enum.reject(&have_license_header?(&1, license_id))
+    |> Enum.each(&insert_license_header(&1, license_id))
   end
 
   defp lib_files do
@@ -32,13 +41,17 @@ defmodule Mix.Tasks.InsertLicense do
     |> Enum.reject(&String.contains?(&1, "lib/mix"))
   end
 
-  defp have_license_header?(filename) do
+  defp have_license_header?(filename, license_id) do
     content = File.read!(filename)
-    String.starts_with?(content, @header)
+    String.starts_with?(content, comment(license_id))
   end
 
-  defp insert_license_header(filename) do
+  defp insert_license_header(filename, license_id) do
     content = File.read!(filename)
-    File.write(filename, [@header, "\n", content])
+    File.write(filename, [comment(license_id), "\n", content])
+  end
+
+  defp comment(license_id) do
+    "# SPDX-License-Identifier: #{license_id}"
   end
 end
