403ea37e8626dd27c9af7b576b5c2dfbb1cc1b71
Samuel <samuel@uniris.io>
2022-03-09 16:49:29 +0100

    Improve mix task sanity checks

--- lib/mix/tasks/insert_license.ex
+++ lib/mix/tasks/insert_license.ex
@@ -1,15 +1,43 @@
 defmodule Mix.Tasks.InsertLicense do
+  @doc false
+
   use Mix.Task
 
   def run(opts \\ []) do
     case OptionParser.parse(opts, strict: [license: :string, verify: :boolean]) do
       {[verify: true, license: license], _argv, _errors} ->
-        verify(String.to_existing_atom(license))
+        case cast(String.upcase(license)) do
+          {:ok, license} ->
+            verify(license)
+          :error ->
+            Mix.shell().error("you pass an invalid license")
+            Mix.shell().info("you should pass 'mit,agpl, gpl, lgpl, mozilla, apache, boost'")
+        end
 
       {[{:license, license} | _], _argv, _errors} ->
-        insert(String.to_existing_atom(license))
+        case cast(String.upcase(license)) do
+          {:ok, license} ->
+            insert(license)
+          :error ->
+            Mix.shell().error("you pass an invalid license")
+            Mix.shell().info("you should pass 'mit,agpl, gpl, lgpl, mozilla, apache, boost'")
+
+        end
+      
+      _ ->
+        Mix.shell().info("You have to pass --license flag and --verify if you can check all the files")
     end
   end
+
+
+  defp cast("MIT"), do: {:ok, :mit}
+  defp cast("AGPL"), do: {:ok, :agpl_v3}
+  defp cast("GPL"), do: {:ok, :gpl_v3}
+  defp cast("LGPL"), do: {:ok, :lgpl_v3}
+  defp cast("MOZILLA"), do: {:ok, :mozilla_v2}
+  defp cast("APACHE"), do: {:ok, :apache_v2}
+  defp cast("BOOST"), do: {:ok, :boost_v1}
+  defp cast(_), do: :error
 
   defp verify(license) do
     files = lib_files()
