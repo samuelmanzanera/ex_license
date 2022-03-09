711220f64e1375504f8164ec217de1c72840857c
Samuel <samuel@uniris.io>
2022-03-09 16:52:34 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -24,9 +24,7 @@ defmodule ExLicense.MixProject do
   defp package do
     [
       name: "ex_license",
-      # These are the default files included in the package
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-        license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/samuelmanzanera/ex_license"}
     ]
